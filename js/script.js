36cf652f2d86121cce34075af067b4825f3dba25
TylerKirumba <tylerkirumba@gmail.com>
2023-11-28 13:23:52 +0300

    1st deployment attempt

--- js/script.js
+++ js/script.js
@@ -50,3 +50,26 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+ /*====================== scroll reveal ======================*/
+ ScrollReveal({ 
+    // reset: true, 
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .education-column, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*====================== typed js ======================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer', 'Graphic Designer', 'Motion graphics animator'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
